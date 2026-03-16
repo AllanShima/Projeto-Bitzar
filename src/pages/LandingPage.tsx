7988d7da826afc3689d2e49cbca070702ef221fe
AllanShima <allanshinhama@gmail.com>
2026-03-16 01:36:18 -0300

    Initial react components and react router

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='w-full h-full bg-amber-300'>
+      hello
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className='w-full h-full'>
+      Page was not found...
+    </div>
+  )
+}
+
+export default NotFoundPage
